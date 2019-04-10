90231be6ed7ecbff9d8c2c7152b009c08af88ec1
AdrianChren <chren.adrian@gmail.com>
2019-04-10 17:47:40 +0200

    Add files via upload

---
+++ nakupuj.js
@@ -0,0 +1,14 @@
+function nakupuj() {
+    var od = document.getElementById('fromInput').value;
+    var kam = document.getElementById('toInput').value;
+    var kedy = document.getElementById('date').value;
+    var ktorej = document.getElementById('time').value;
+
+     
+}
+
+
+
+//window.open('https://ikvc.slovakrail.sk/esales/search','_blank');
+//document.getElementById("#fromInput").innerHTML = od;
+//document.getElementById("#toInput").innerHTML = kam;
